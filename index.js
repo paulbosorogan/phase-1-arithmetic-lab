f3ad5ea5b368c80ae0e38b325a64f61f51627cab
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-09-21 19:00:38 -0400

    commit tree 4b1d5cff58a71f3485cd2d38c492c2bf6873feb4

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+multiply = 62;
+
+let random = 0;
+random = Math.floor(Math.random()*10) +1;
+
+let num3 = 80;
+let num4 = 76;
+let mod = num3 % num4;
+mod = 4;
+
+let max = 0;
+max = Math.max(1,2,3,4,5,20);
